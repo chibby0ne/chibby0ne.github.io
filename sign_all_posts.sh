58c83c1fdd9895ff196b51a0d8e93c65c64d6f84
Antonio Gutierrez <chibby0ne@gmail.com>
2018-05-08 15:10:04 +0200

    sign_all_posts: refactor to only sign new posts
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- sign_all_posts.sh
+++ sign_all_posts.sh
@@ -2,5 +2,7 @@
 
 for post in _posts/*.md; do
     postfile=$(basename ${post})
-    keybase pgp sign -i $post > _posts_signatures/${postfile%%.md}.asc
+    if [[ ! -f  _posts_signatures/${postfile%%.md}.asc ]]; then
+        keybase pgp sign -i $post > _posts_signatures/${postfile%%.md}.asc
+    fi
 done
