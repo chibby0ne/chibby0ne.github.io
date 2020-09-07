9ea3b1b76eae3e455c61aa7427f9c8acae009446
Antonio Gutierrez <chibby0ne@gmail.com>
2020-09-08 00:06:41 +0200

    rename _posts_signatures to _posts_signatures
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- sign_all_posts.sh
+++ sign_all_posts.sh
@@ -2,7 +2,7 @@
 
 for post in content/posts/*.md; do
     postfile=$(basename ${post})
-    signature=_posts_signatures/${postfile%%.md}.asc
+    signature=posts_signatures/${postfile%%.md}.asc
     if [[ ${post} -nt ${signature} ]]; then
         echo "post: ${postfile} was modified after the signature creation or signature doesn't exist yet, therefore need to (re)generate that post's signature"
         keybase pgp sign -i $post > ${signature}
